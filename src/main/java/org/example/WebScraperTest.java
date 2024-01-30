f03a4fe6772db67f4aef95cf44a7065157a799e7
Eric.Lee <a23034@athena.com.tw>
2024-01-30 17:35:05 +0800

    響應攔截器

--- src/main/java/org/example/WebScraperTest.java
+++ src/main/java/org/example/WebScraperTest.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -21,15 +25,27 @@ public class WebScraperTest implements CommandLineRunner {
         //設定ChromeOptions，允許跨域
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-
+        // 創建並啟動BrowserMob Proxy
+        BrowserMobProxy proxy = new BrowserMobProxyServer();
+        // 0 表示選擇一個隨機可用的端口
+        proxy.start(0);
+        // 獲取代理服務器的Selenium代理對象
+        Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+        // 配置Selenium以使用代理
+        options.setProxy(seleniumProxy);
         //Selenium對不同瀏覽器提供了不同的webDriver
         WebDriver driver = new ChromeDriver(options);
-        // 到Google首頁
-        driver.get("https://www.google.com.tw/");
-        // 取得pageTitle
-        String title = driver.getTitle();
-        System.out.print(title);
-  }
+
+        // 設置響應攔截器
+        proxy.addResponseFilter((response, contents, messageInfo) -> {
+            // 到Google首頁
+            driver.get("https://www.google.com.tw/");
+            // 取得pageTitle
+            String title = driver.getTitle();
+            System.out.print(title);
+        });
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebScraperTest.class, args);
