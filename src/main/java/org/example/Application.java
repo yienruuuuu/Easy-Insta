65ec0b20ea41b802287721e30c4d53cbb01220a6
Eric.Lee <a23034@athena.com.tw>
2024-02-01 11:33:06 +0800

    規範資料夾格式 服務啟用入口 db配置

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication(scanBasePackages = {"org.example.*"})
+@EnableTransactionManagement
+@EnableScheduling
+@EnableCaching
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
