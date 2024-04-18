43d9b8024acab6971594da38e070831cc9a509dc
Eric.Lee <c6b7yntz@gmail.com>
2024-04-18 13:21:00 +0800

    優化導出報表

--- src/main/java/org/example/controller/ExcelController.java
+++ src/main/java/org/example/controller/ExcelController.java
@@ -8,12 +8,14 @@ import org.example.exception.ApiException;
 import org.example.exception.SysCode;
 import org.example.service.ExcelService;
 import org.example.service.IgUserService;
+import org.example.service.MediaCommentService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Slf4j
 @Tag(name = "Excel controller", description = "開發導出Excel用API")
@@ -22,10 +24,12 @@ import javax.servlet.http.HttpServletResponse;
 public class ExcelController extends BaseController {
     private final IgUserService igUserService;
     private final ExcelService excelService;
+    private final MediaCommentService mediaCommentService;
 
-    public ExcelController(IgUserService igUserService, ExcelService excelService) {
+    public ExcelController(IgUserService igUserService, ExcelService excelService, MediaCommentService mediaCommentService) {
         this.igUserService = igUserService;
         this.excelService = excelService;
+        this.mediaCommentService = mediaCommentService;
     }
 
     @GetMapping(value = "/getUserCanExport")
